9d25d0717b381dc52352a1983f97715b9884031f
Erick Hernandez <erieduhergue11@gmail.com>
2019-03-27 08:29:25 -0700

    Navbar and the header parallax, content.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,8 @@
+// Call to parallax function, materialize
+$('.parallax').parallax();
 
+// Call to dropdown function, materialize
+$('.dropdown-trigger').dropdown();
+
+// Call to sidenav function, materialize
+$('.sidenav').sidenav();
